065371f33bdd27241d6089ccc1e01719d55d7698
Ken <kabutukenneth3@gmail.com>
2023-03-28 06:14:19 +0300

    js assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+let multiply
+let num1 = 31
+let num2 = 2
+
+multiply = num1 * num2 
+multiply === 62
+
+// random
+//let random
+//(Number.isInteger(random))
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+//mod
+let mod
+let num3 = 16
+let num4 = 4
+mod = num3 / num4
+
+//max
+//let max
+//max = math.max(1, 5, 12, 20, 19, 17)
+const numbers = [10, 5, 20, 15];
+const max = Math.max(...numbers);
